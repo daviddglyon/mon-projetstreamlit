b0afe3ff0a787cac741a0bafc6a3c2401a23d4ab
daviddglyon <davidgogia@ymail.com>
2026-08-26 09:43:05 +0200

    add dashboard page

--- app_securisee.py
+++ app_securisee.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+import matplotlib.pyplot as plt
+
 # --- 1. INITIALISATION DE LA SESSION ---
 if "logged_in" not in st.session_state:
     st.session_state["logged_in"] = False
@@ -49,7 +51,7 @@ else:
     with st.sidebar:
         selected_page = option_menu(
             menu_title="Menu principal",
-            options=["Accueil", "Galerie Photos"],
+            options=["Accueil", "Dashboard", "Galerie Photos"],
             icons=["house", "images"],
             default_index=0
         )
@@ -58,20 +60,69 @@ else:
     if selected_page == "Accueil":
         st.title(" Page d'Accueil Réservée")
         st.write("Ce contenu est uniquement accessible aux utilisateurs authentifiés.")
+    elif selected_page == "Dashboard":
+        st.title("Dashboard")
+        st.write("Voici nos Dashboards")   
+        ## includ dashboard start________________________________________
+
+        # 1. Sélection du jeu de données
+        dataset_name = "flights"
+
+        # Chargement dynamique
+        @st.cache_data
+        def get_dataset(name):
+            return pd.read_csv(f"https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/{ name }.csv")
+        df = get_dataset(dataset_name)
+
+        #3. Permettez à l'utilisateur de :
+        # Filtrer les données sur une plage d'années à l'aide d'un st.slider (ex: entre 1949 et 1960).
+        annees = st.slider("plage d'années", min_value=1949, max_value=1960, step=1)
+        # Choisir un mois spécifique via un st.selectbox ou sélectionner tous les mois.
+        mois = st.selectbox("mois: ", ("January","February","March","April","May", "June","July","August","September","October","November","December"))
+        st.write(mois)
+
+        #4. Affichez :
+
+        ## Un indicateur st.metric montrant le nombre total de passagers sur la période sélectionnée.
+        sel_data = df.loc[(df["year"]==annees) & (df["month"]==mois)]
+        passangers = st.metric(label="le nombre total de passagers sur la période sélectionnée", value=sel_data["passengers"])
+
+        ## Un graphique évolutif en lignes (st.line_chart ou Plotly) du nombre de passagers au fil des mois/années.
+        passagers_annee= df.loc[(df["year"]==annees)]
+        st.line_chart(passagers_annee["passengers"])
+
+        st.divider()
+
+        ## Une case à cocher (st.checkbox) qui, lorsqu'elle est activée, affiche la Heatmap Seaborn montrant la répartition des passagers par année et par mois (utilisez df.pivot(index='month', columns='year', values='passengers')).
+        import plotly.express as px
+        import seaborn as sns
+        if st.checkbox("heatmap") :
+            pivot_df = df.pivot(index='month', columns='year', values='passengers')
+            num_cols = pivot_df.select_dtypes(include=['float64','int64']).columns.tolist()
+
+            st.subheader("corrélation")
+            corr = pivot_df[num_cols].corr()
+
+            fig_corr, ax_corr = plt.subplots(figsize=(6, 4))
+            
+            sns.heatmap(corr, annot=True, cmap="coolwarm", fmt=".2f",ax=ax_corr)
+            st.pyplot(fig_corr)
+
+        ## includ dashboard end________________________________________
     elif selected_page == "Galerie Photos":
         st.title(" Album Photos")
         st.write("Voici la galerie multimédia organisée sur 3 colonnes:")
 
-    # Exemple d'affichage d'images alignées sur 3 colonnes
-    cols = st.columns(3)
-    sample_images = [
-        "https://static.streamlit.io/examples/cat.jpg",
-        "https://static.streamlit.io/examples/dog.jpg",
-        "https://static.streamlit.io/examples/owl.jpg"
-    ]
-    for idx, img_url in enumerate(sample_images):
-        with cols[idx % 3]:
-            st.image(img_url, caption=f"Image {idx+1}", use_container_width=True)
+        # Exemple d'affichage d'images alignées sur 3 colonnes
+        cols = st.columns(3)
+        sample_images = [
+            "https://static.streamlit.io/examples/cat.jpg",
+            "https://static.streamlit.io/examples/dog.jpg",
+            "https://static.streamlit.io/examples/owl.jpg"
+        ]
+        for idx, img_url in enumerate(sample_images):
+            with cols[idx % 3]:
+                st.image(img_url, caption=f"Image {idx+1}", use_container_width=True)
 
 
 
